126b20d74afbf21e1e0c680acc95a443b13674b6
lucas <Rodriguesdesouzal28@gmail.com>
2024-07-24 19:04:28 -0300

    feat: :sparkles: services

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import styles from "./page.module.css";
 import NavBar from "@/components/nav-bar";
 import { Box } from "@chakra-ui/react";
 import Hero from "../components/hero";
+import Services from "@/components/services";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
+      <Services />
     </main>
   );
 }
---
+++ components/services.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { Flex, Image, Text } from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+import { Pagination } from "swiper/modules";
+import useWindowSize from "@/hooks/useWindowSize";
+
+export default function Services() {
+  const { width } = useWindowSize();
+
+  return (
+    <Flex w={"100%"} pb={50} direction={"column"} >
+      <Text
+        color={"#062126"}
+        fontFamily={"'Poppins', sans-serif"}
+              fontSize={{ base: "16px", md: "48px" }}
+              mb={15}
+      >
+        Nossos serviços
+      </Text>
+      <Swiper
+        slidesPerView={width < 1024 ? 2 : 3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="mySwiperSlide">
+          <Flex direction={"column"} alignItems={"flex-start"}>
+            <Image src="assets/services/Rectangle5.png" alt="service" />
+            <Text
+              mt={5}
+              color={"#062126"}
+              fontFamily={"'Poppins', sans-serif"}
+              fontSize={{ base: "16px", md: "24px" }}
+            >
+              Cuidado imediato
+            </Text>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide className="mySwiperSlide">
+          <Flex direction={"column"} alignItems={"flex-start"}>
+            <Image src="assets/services/Rectangle6.png" alt="service" />
+            <Text
+              mt={5}
+              color={"#062126"}
+              fontFamily={"'Poppins', sans-serif"}
+              fontSize={{ base: "16px", md: "24px" }}
+            >
+              Cuidado dental
+            </Text>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide className="mySwiperSlide">
+          <Flex direction={"column"} alignItems={"flex-start"}>
+            <Image src="assets/services/Rectangle7.png" alt="service" />
+            <Text
+              mt={5}
+              color={"#062126"}
+              fontFamily={"'Poppins', sans-serif"}
+              fontSize={{ base: "16px", md: "24px" }}
+            >
+              Dental
+            </Text>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide className="mySwiperSlide">
+          <Flex direction={"column"} alignItems={"flex-start"}>
+            <Image src="assets/services/Rectangle7.png" alt="service" />
+            <Text
+              mt={5}
+              color={"#062126"}
+              fontFamily={"'Poppins', sans-serif"}
+              fontSize={{ base: "16px", md: "24px" }}
+            >
+              Centro de diagnóstico
+            </Text>
+          </Flex>
+        </SwiperSlide>
+      </Swiper>
+    </Flex>
+  );
+}
